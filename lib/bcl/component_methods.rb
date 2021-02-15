655120f0047e917fd149b50ed8d4be8db34bbaeb
Nicholas Long <nicholas.long@nrel.gov>
2021-02-15 11:31:59 -0700

    update copyright to 2021

--- lib/bcl/component_methods.rb
+++ lib/bcl/component_methods.rb
@@ -1,5 +1,5 @@
 ######################################################################
-#  Copyright (c) 2008-2019, Alliance for Sustainable Energy.
+#  Copyright (c) 2008-2021, Alliance for Sustainable Energy.
 #  All rights reserved.
 #
 #  This library is free software; you can redistribute it and/or
